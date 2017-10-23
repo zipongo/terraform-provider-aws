dfd88f061ecbe893dc9c1856f57e6bc7cc62cc47
Radek Simko <radek.simko@gmail.com>
2017-10-23 16:47:59 +0100

    r/cognito_identity_pool_domain: Add resource

--- aws/provider.go
+++ aws/provider.go
@@ -277,6 +277,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_config_delivery_channel":                  resourceAwsConfigDeliveryChannel(),
 			"aws_cognito_identity_pool":                    resourceAwsCognitoIdentityPool(),
 			"aws_cognito_user_pool":                        resourceAwsCognitoUserPool(),
+			"aws_cognito_user_pool_domain":                 resourceAwsCognitoUserPoolDomain(),
 			"aws_autoscaling_lifecycle_hook":               resourceAwsAutoscalingLifecycleHook(),
 			"aws_cloudwatch_metric_alarm":                  resourceAwsCloudWatchMetricAlarm(),
 			"aws_cloudwatch_dashboard":                     resourceAwsCloudWatchDashboard(),
